17666edd25f84a8243cb40a889e77fa3deaea880
of-curso-python <of.curso.python@gmail.com>
2017-06-20 13:59:45 -0600

    ejercicios diccionario

---
+++ ejercicios/diccionario_ejercicio_0.py
@@ -0,0 +1,18 @@
+"""
+1. Crear un script para
+ - Recibir args ‘nombre’, ‘edad’, ‘clase’ y
+   crear un diccionario de la forma:
+       estudiante = {
+           'nombre': <nombre>,
+           'edad': <edad>,
+           'curso': <curso>
+       }
+
+ Imprimir diccionario ( print estudiante )
+ Imprimir las llaves del diccionario y los valores
+
+2. Crear un script para:
+   Recibir un nombre y verificar si existe en el diccionario dado.
+   'La llave <nombre> existe en el diccionario generado.' o
+   'La llave <nombre> no existe en el diccionario'
+"""
